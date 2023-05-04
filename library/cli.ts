b1fbd1c5f676edf70d2ed8aae0481cafd4cdf3a8
Shiba <44804845+DeepDoge@users.noreply.github.com>
2023-05-04 09:49:35 +0000

    oops

--- library/cli.ts
+++ library/cli.ts
@@ -3,6 +3,6 @@
 import { generate } from "./generate"
 import args from "command-line-args"
 
-const { contractsDirPath } = args([{ name: "source", alias: "s", type: String }])
+const contractsDirPath = args([{ name: "src", type: String }]).src
 if (!contractsDirPath) throw new Error("You didn't provide a path for contracts directory.")
 generate(contractsDirPath)
