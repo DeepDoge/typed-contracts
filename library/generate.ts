acc14dc30aec23c3030a90053551a89c78fbbb7b
Shiba <44804845+DeepDoge@users.noreply.github.com>
2023-05-09 10:53:40 +0000

    now it should work recursively too

--- library/generate.ts
+++ library/generate.ts
@@ -1,32 +1,42 @@
 import fs from "fs"
-import { compileAndSave } from "waffle-compiler/library/compiler"
+import { compileAndSave } from "waffle-compiler/library"
 import path from "path"
+import type { Abi } from "./types/abi"
 
-export async function generate(contractsDirPath: string) {
-	const contractFilenames = fs.readdirSync(contractsDirPath).filter((filename) => filename.endsWith(".sol"))
-
-	const artifactsDirPath = path.join(contractsDirPath, "artifacts")
+export async function generate(contractsDirpath: string) {
+	const artifactsDirpath = path.join(contractsDirpath, "artifacts")
 
 	console.log("Generating abi files...")
 	await compileAndSave({
-		sourceDirectory: contractsDirPath,
-		outputDirectory: artifactsDirPath,
+		sourceDirectory: contractsDirpath,
+		outputDirectory: artifactsDirpath,
 	})
 	console.log("Generated abi files ")
 	console.log()
 
-	for (const filename of contractFilenames) {
-		const contractFilePath = path.resolve(path.join(contractsDirPath, filename))
-		const name = path.basename(contractFilePath, ".sol")
+	const dirents = fs.readdirSync(artifactsDirpath, { recursive: true, encoding: "utf-8", withFileTypes: true })
+	for (const dirent of dirents) {
+		if (!dirent.isFile()) continue
+		if (!dirent.name.endsWith(".json")) continue
+
+		const fullpath = path.join(artifactsDirpath, dirent.name)
+
+		let abi: Abi
+		try {
+			const jsonData = JSON.parse(fs.readFileSync(fullpath).toString()) as unknown
+			if (!(jsonData && typeof jsonData === "object" && "abi" in jsonData && Array.isArray(jsonData.abi)))
+				throw new Error("Json file is doesn't contain abi.")
+			abi = jsonData.abi
+		} catch (error) {
+			console.warn("Error while parsing", fullpath, error)
+			continue
+		}
+
+		const name = path.basename(fullpath, ".json")
+		const dirname = path.dirname(fullpath)
 		const tsFilename = `${name}.ts`
 		console.log(`Generating ${tsFilename}...`)
 
-		const jsonFilePath = path.join(artifactsDirPath, `${name}.json`)
-		const json = fs.readFileSync(jsonFilePath).toString()
-		const jsonObj = JSON.parse(json)
-		if (!(jsonObj && typeof jsonObj === "object" && "abi" in jsonObj && Array.isArray(jsonObj.abi)))
-			throw new Error("Json file is doesn't contain abi.")
-		const abi = jsonObj.abi
 		const type = [
 			`import type { TypifyContract } from "typify-contracts/library/types/typify"`,
 			`import { Contract, type ContractRunner } from "ethers"`,
@@ -34,10 +44,11 @@ export async function generate(contractsDirPath: string) {
 			`export const connect_${name} = (address: string, runner: ContractRunner) => new Contract(address, abi, runner) as ${name}_Contract`,
 			`const abi = ${JSON.stringify(abi, null, "\t")} as const`,
 		].join("\n")
-		fs.writeFileSync(path.join(artifactsDirPath, tsFilename), type)
+		fs.writeFileSync(path.join(dirname, tsFilename), type)
 
-		console.log(`Generated ${filename}`)
+		console.log(`Generated ${tsFilename}`)
 	}
+
 	console.log()
 	console.log("Done")
 }
