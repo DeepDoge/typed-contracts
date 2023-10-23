d833681a77cfcc4afc793df45924aa90479dea9b
Shiba <44804845+DeepDoge@users.noreply.github.com>
2023-10-23 10:43:20 +0000

    hmm

--- bin/cli.ts
+++ bin/cli.ts
@@ -1,8 +1,6 @@
-#!/usr/bin/env tsx
-
-import { generate } from "./generate"
 import args from "command-line-args"
+import { generate } from "../library/generate"
 
-const contractsDirPath = args([{ name: "src", type: String }]).src
+const contractsDirPath = args([{ name: "src", type: String }])["src"] as string | undefined
 if (!contractsDirPath) throw new Error("You didn't provide a path for contracts directory.")
 generate(contractsDirPath)
--- library/generate.ts
+++ library/generate.ts
@@ -1,5 +1,5 @@
-import fs from "fs"
-import path from "path"
+import fs from "node:fs"
+import path from "node:path"
 import { compileAndSave } from "waffle-compiler/library/compiler"
 import type { Abi } from "./types/abi"
 
