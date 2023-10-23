4291e32c8c560335c6d68a35c2171a3143ba3418
Shiba <44804845+DeepDoge@users.noreply.github.com>
2023-10-23 11:14:26 +0000

    ...

--- cli.ts
+++ cli.ts
@@ -1,5 +1,5 @@
 import args from "command-line-args"
-import { generate } from "../library/generate"
+import { generate } from "./library/generate"
 
 const contractsDirPath = args([{ name: "src", type: String }])["src"] as string | undefined
 if (!contractsDirPath) throw new Error("You didn't provide a path for contracts directory.")
